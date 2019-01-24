800b6f6ade6c1bacf1793426bf0e26339106b8da
Omar <oomarusman@gmail.com>
2019-01-24 23:33:09 +0800

    Add TodoController.

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Http\Contracts\TodoRepositoryInterface;
+use App\Http\Repositories\TodoRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app->bind(TodoRepositoryInterface::class, TodoRepository::class);
     }
 
     /**
